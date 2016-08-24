4dd9f7e2d6cbd99be123eecf6651cd14562dbb0d
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-08-24 16:53:46 -0400

    normalized thumbnail heights in realisations page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,17 @@
 $(".container").hide();
 $(document).ready(function() {
     $('.container').fadeIn();
+    equalHeight($('.realisation .thumbnail'));
+
 });
+
+function equalHeight(group) {    
+    var tallest = 0;    
+    group.each(function() {       
+        var thisHeight = $(this).height();       
+        if(thisHeight > tallest) {          
+            tallest = thisHeight;       
+        }    
+    });    
+    group.each(function() { $(this).height(tallest); });
+} 
